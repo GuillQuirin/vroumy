66256b9f90cbc970850ab09adeef14de277cc914
laurieguibert <l.guibert@hotmail.fr>
2017-06-07 21:59:57 +0200

    Fichiers de conf MAJ auto

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  resources :constructeurs
+  resources :voitures
+	root :to => 'vroumy#index'
+  resources :utilisateurs
   resources :vroumies
+  resources :events
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
