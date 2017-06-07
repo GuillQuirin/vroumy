abb707da16f8ef7e5018ff252ad1aec17e4724b0
laurieguibert <l.guibert@hotmail.fr>
2017-06-07 22:02:16 +0200

    Ajout fichiers divers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+	def notice_name(name)
+		return :info unless %w(info success warning danger).include?(name)
+		name
+	end
+	def date_format(date)
+		date.strftime("%d/%m/%Y %I:%M:%S")
+	end
 end
