1b26d8a7f1539d08a01900f09c89f828c05d08ca
laurieguibert <l.guibert@hotmail.fr>
2017-06-07 21:57:54 +0200

    Modif controller et vue utilisateur

--- app/controllers/utilisateurs_controller.rb
+++ app/controllers/utilisateurs_controller.rb
@@ -2,13 +2,11 @@ class UtilisateursController < ApplicationController
   before_action :set_utilisateur, only: [:show, :edit, :update, :destroy]
 
   # GET /utilisateurs
-  # GET /utilisateurs.json
   def index
     @utilisateurs = Utilisateur.all
   end
 
   # GET /utilisateurs/1
-  # GET /utilisateurs/1.json
   def show
   end
 
@@ -22,43 +20,29 @@ class UtilisateursController < ApplicationController
   end
 
   # POST /utilisateurs
-  # POST /utilisateurs.json
   def create
     @utilisateur = Utilisateur.new(utilisateur_params)
-
-    respond_to do |format|
-      if @utilisateur.save
-        format.html { redirect_to @utilisateur, notice: 'Utilisateur was successfully created.' }
-        format.json { render :show, status: :created, location: @utilisateur }
-      else
-        format.html { render :new }
-        format.json { render json: @utilisateur.errors, status: :unprocessable_entity }
-      end
+    if @utilisateur.save
+      flash[:notice] = "L'utilisateur #{@utilisateur.name} a bien été créé."
+      redirect_to @voiture
+    else
+      render 'new'
     end
   end
 
-  # PATCH/PUT /utilisateurs/1
-  # PATCH/PUT /utilisateurs/1.json
   def update
-    respond_to do |format|
-      if @utilisateur.update(utilisateur_params)
-        format.html { redirect_to @utilisateur, notice: 'Utilisateur was successfully updated.' }
-        format.json { render :show, status: :ok, location: @utilisateur }
-      else
-        format.html { render :edit }
-        format.json { render json: @utilisateur.errors, status: :unprocessable_entity }
-      end
+    if @utilisateur.update(utilisateur_params)
+      flash[:notice] = "L'utilisateur #{@utilisateur.name} a bien été mis à jour."
+      redirect_to @utilisateur
+    else
+      render 'edit'
     end
   end
 
   # DELETE /utilisateurs/1
-  # DELETE /utilisateurs/1.json
   def destroy
     @utilisateur.destroy
-    respond_to do |format|
-      format.html { redirect_to utilisateurs_url, notice: 'Utilisateur was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to utilisateurs_path
   end
 
   private
@@ -69,6 +53,15 @@ class UtilisateursController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def utilisateur_params
-      params.require(:utilisateur).permit(:firstName, :lastName, :email, :numAddress, :address, :city, :phone, :isActive)
+      params.require(:utilisateur).permit(
+        :firstName, 
+        :lastName, 
+        :email, 
+        :numAddress, 
+        :address, 
+        :city, 
+        :phone, 
+        :isActive
+      )
     end
 end
